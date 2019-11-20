ab5eda7f19cc7e6d415e9246f2ccb2c82b249ca3
ullasrao <rao.ullas@gmail.com>
2019-11-20 11:35:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,4 +3,6 @@ echo "<img src='abc.png' alt='abc' />";
 $connection = new PDO('mysql:host=localhost;dbname=demo', 'demo', 'demo');
 $statement  = $connection->query('SELECT message FROM demo');
 
+
+
 echo $statement->fetchColumn();
